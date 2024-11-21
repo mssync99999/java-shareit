f835daf07bbf7427cbdaaaffe0061b8ab2c7ca79
Boris <mssync99999@gmail.com>
2024-11-21 19:29:08 +0300

    21112024-1929

--- src/main/java/ru/practicum/shareit/item/dao/ItemRepository.java
+++ src/main/java/ru/practicum/shareit/item/dao/ItemRepository.java
@@ -1,8 +1,6 @@
 package ru.practicum.shareit.item.dao;
 
-import ru.practicum.shareit.item.dto.ItemDto;
 import ru.practicum.shareit.item.model.Item;
-
 import java.util.List;
 import java.util.Optional;
 
