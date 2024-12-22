6e5a7dd386eb638ed668b2ae90bdec622223a445
Boris <mssync99999@gmail.com>
2024-12-22 21:35:23 +0300

    22122024-2135

--- server/src/test/java/ru/practicum/shareit/ShareItTests.java
+++ server/src/test/java/ru/practicum/shareit/ShareItTests.java
@@ -11,10 +11,3 @@ class ShareItTests {
 	}
 
 }
-
-/*
-        <dependency>
-            <groupId>org.springframework.boot</groupId>
-            <artifactId>spring-boot-starter-validation</artifactId>
-        </dependency>
- */
